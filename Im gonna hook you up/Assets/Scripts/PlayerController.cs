0b9eae66e80daf89816e4bf8e549263d0a8ef244
mmeo009 <shh020615@gmail.com>
2023-09-13 00:23:42 +0900

    락시때 만듬

--- Im gonna hook you up/Assets/Scripts/PlayerController.cs
+++ Im gonna hook you up/Assets/Scripts/PlayerController.cs
@@ -14,8 +14,45 @@ public class PlayerController : MonoBehaviour
         Dance
     }
 
+    public State state = State.None;
+    public bool isPlay = false;
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
-
+        if(Input.GetKey(KeyCode.K))
+        {
+            state = State.Throw;
+        }
+        else
+        {
+            state = State.None;
+        }
+    }
+    IEnumerator KiwiAction()
+    {
+        while (!isPlay)
+        {
+            switch (state)
+            {
+                case State.None:
+                    anim.Play("None");
+                    break;
+                case State.Throw:
+                    anim.Play("Throw");
+                    break;
+                case State.Wait:
+                    break;
+                case State.Hook:
+                    break;
+                case State.Dance:
+                    break;
+            }
+        }
+        yield return new WaitForSeconds(0.1f);
     }
 }
