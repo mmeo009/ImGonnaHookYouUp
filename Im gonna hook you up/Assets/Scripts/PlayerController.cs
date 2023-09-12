c2a03e67385014b7c2b255437b22d858d105d0c9
mmeo009 <shh020615@gmail.com>
2023-09-12 12:16:58 +0900

    아니 코드를 5줄썼대

---
+++ Im gonna hook you up/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public enum State
+    {
+        None,
+        Throw,
+        Wait,
+        Hook,
+        Dance
+    }
+
+    void Update()
+    {
+
+    }
+}
